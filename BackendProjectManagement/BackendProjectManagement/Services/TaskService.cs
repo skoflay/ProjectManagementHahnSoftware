af2e0c352b1085b1c8e6a4811b1fbf2576d6004b
souhail <boullilsouhil@gmail.com>
2025-12-15 23:50:54 +0100

    Create Task method

--- BackendProjectManagement/BackendProjectManagement/Services/TaskService.cs
+++ BackendProjectManagement/BackendProjectManagement/Services/TaskService.cs
@@ -1,4 +1,5 @@
-﻿using BackendProjectManagement.Repositories;
+﻿using BackendProjectManagement.Models;
+using BackendProjectManagement.Repositories;
 
 namespace BackendProjectManagement.Services
 {
@@ -9,6 +10,13 @@ namespace BackendProjectManagement.Services
         public TaskService(ITaskRepository taskRepository)
         {
             _taskRepository = taskRepository;
+        }
+
+        public async Task<TaskItem> CreateAsync(TaskItem task)
+        {
+            task.IsCompleted = false;
+            await _taskRepository.AddAsync(task);
+            return task;
         }
     }
 }
