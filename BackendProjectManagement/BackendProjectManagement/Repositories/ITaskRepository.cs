b29785d27a1d00fb2987058e8b4ed07a70558bba
souhail <boullilsouhil@gmail.com>
2025-12-15 23:28:30 +0100

    Add Task Repository

---
+++ BackendProjectManagement/BackendProjectManagement/Repositories/ITaskRepository.cs
@@ -0,0 +1,14 @@
+﻿using BackendProjectManagement.Models;
+
+namespace BackendProjectManagement.Repositories
+{
+    public interface ITaskRepository
+    {
+        Task<TaskItem?> GetByIdAsync(int id);
+        Task<List<TaskItem>> GetByProjectIdAsync(int projectId);
+        Task AddAsync(TaskItem task);
+        Task UpdateAsync(TaskItem task);
+        Task DeleteAsync(TaskItem task);
+    }
+
+}
