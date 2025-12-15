b3db49d0be727276ba63aa5a004d2d89ea14a176
souhail <boullilsouhil@gmail.com>
2025-12-15 20:32:44 +0100

    to dto method

--- BackendProjectManagement/BackendProjectManagement/Mapper/ProjectMapper.cs
+++ BackendProjectManagement/BackendProjectManagement/Mapper/ProjectMapper.cs
@@ -1,6 +1,40 @@
-﻿namespace BackendProjectManagement.Mapper
+﻿using BackendProjectManagement.DTOs;
+using BackendProjectManagement.Models;
+
+namespace BackendProjectManagement.Mapper
 {
-    public class ProjectMapper
+    public static class ProjectMapper
     {
+
+        public static Project ToEntity(CreateProjectDto dto)
+        {
+            return new Project
+            {
+                Id = Guid.NewGuid(),       
+                Title = dto.Title,
+                Description = dto.Description,
+                CreatedAt = DateTime.UtcNow
+            };
+        }
+
+
+        public static void UpdateEntity(Project project, UpdateProjectDto dto)
+        {
+            project.Title = dto.Title;
+            project.Description = dto.Description;
+        }
+
+
+        public static CreateProjectDto ToDto(Project project)
+        {
+            return new CreateProjectDto
+            {
+                Title = project.Title,
+                Description = project.Description
+            };
+        }
+
+
+
     }
 }
