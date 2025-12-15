ea8b2a89802bf535b80e00395541a375a5d39122
souhail <boullilsouhil@gmail.com>
2025-12-15 18:07:44 +0100

    Initial Commit

---
+++ BackendProjectManagement/BackendProjectManagement/Controllers/TaskController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendProjectManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+    }
+}
