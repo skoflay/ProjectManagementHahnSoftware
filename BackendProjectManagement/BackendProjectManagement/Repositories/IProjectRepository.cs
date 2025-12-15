fe3ebc853da02e78e9b5a87776fed33ec4489e3b
souhail <boullilsouhil@gmail.com>
2025-12-15 19:11:38 +0100

    add Interface project repository

---
+++ BackendProjectManagement/BackendProjectManagement/Repositories/IProjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using BackendProjectManagement.Models;
+
+namespace BackendProjectManagement.Repositories
+{
+    public interface IProjectRepository
+    {
+        Task<Project> AddAsync(Project project);
+        Task<List<Project>> GetAllAsync();
+        Task<Project?> GetByIdAsync(Guid id);
+        Task<bool> UpdateAsync(Project project);
+        Task<bool> DeleteAsync(Guid id);
+    }
+
+}
