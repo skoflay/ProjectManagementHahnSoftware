4cd4e139c5b673ed22bf569adaf7a85db9216910
souhail <boullilsouhil@gmail.com>
2025-12-16 00:16:08 +0100

    injection TaskService

---
+++ BackendProjectManagement/BackendProjectManagement/Controllers/TasksController.cs
@@ -0,0 +1,23 @@
+﻿using BackendProjectManagement.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendProjectManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly ITaskService _taskService;
+
+        public TasksController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+
+
+
+
+    }
+}
