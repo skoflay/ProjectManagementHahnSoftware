7b8a99996a7f7ae580e7c5c17ce6a5ad72b5da8d
souhail <boullilsouhil@gmail.com>
2025-12-15 18:44:30 +0100

    Add AppDbContext & EntityFramework for MySql

---
+++ BackendProjectManagement/BackendProjectManagement/Data/AppDbContext .cs
@@ -0,0 +1,25 @@
+﻿using BackendProjectManagement.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace BackendProjectManagement.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Project> Projects => Set<Project>();
+        public DbSet<TaskItem> Tasks => Set<TaskItem>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Project>()
+                .HasMany(p => p.Tasks)
+                .WithOne(t => t.Project)
+                .HasForeignKey(t => t.ProjectId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+
+}
