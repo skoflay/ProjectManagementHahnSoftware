ceb6a5b4e6931b449f0b3b07e4a30a9a12858810
souhail <boullilsouhil@gmail.com>
2025-12-15 20:47:39 +0100

    minor changes

--- BackendProjectManagement/BackendProjectManagement/Program.cs
+++ BackendProjectManagement/BackendProjectManagement/Program.cs
@@ -1,6 +1,60 @@
+using BackendProjectManagement.Data;
+using BackendProjectManagement.Repositories;
+using BackendProjectManagement.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+
+//MYQL Connection configuration
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    options.UseMySql(
+        connectionString,
+        ServerVersion.AutoDetect(connectionString)
+    );
+});
+
+// DI
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+builder.Services.AddScoped<IProjectService, ProjectService>();
+
+
+
+// jwt Authentication configuration
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(jwtSettings["Key"]!)
+        )
+    };
+});
+
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
--- BackendProjectManagement/BackendProjectManagement/Services/IProjectService.cs
+++ BackendProjectManagement/BackendProjectManagement/Services/IProjectService.cs
@@ -1,6 +1,14 @@
-﻿namespace BackendProjectManagement.Services
+﻿using BackendProjectManagement.Models;
+
+namespace BackendProjectManagement.Services
 {
     public interface IProjectService
     {
+        Task<Project> CreateAsync(Project project);
+        Task<List<Project>> GetAllAsync();
+        Task<bool> UpdateAsync(Project project);
+        Task<bool> DeleteAsync(Guid id);
+        Task<Project?> GetByIdAsync(Guid id);
+
     }
 }
