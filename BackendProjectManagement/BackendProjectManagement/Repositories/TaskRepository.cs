93cd5186a9c1fb6b9a79701e98c1d009be704349
souhail <boullilsouhil@gmail.com>
2025-12-15 23:42:38 +0100

    getTaskOfProject

--- BackendProjectManagement/BackendProjectManagement/Repositories/TaskRepository.cs
+++ BackendProjectManagement/BackendProjectManagement/Repositories/TaskRepository.cs
@@ -1,6 +1,33 @@
-﻿namespace BackendProjectManagement.Repositories
+﻿using BackendProjectManagement.Data;
+using BackendProjectManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendProjectManagement.Repositories
 {
-    public class TaskRepository
+    public class TaskRepository : ITaskRepository
     {
+        private readonly AppDbContext _context;
+
+        public TaskRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TaskItem?> GetByIdAsync(Guid id)
+        {
+            return await _context.Tasks
+                .FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<List<TaskItem>> GetByProjectIdAsync(Guid projectId)
+        {
+            return await _context.Tasks
+                .Where(t => t.ProjectId == projectId)
+                .ToListAsync();
+        }
+
+
+
+
     }
 }
