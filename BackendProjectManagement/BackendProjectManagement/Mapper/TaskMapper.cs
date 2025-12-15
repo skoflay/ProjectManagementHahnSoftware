61aeaa8f80eb39b906cc4070045fb6ff7606c036
souhail <boullilsouhil@gmail.com>
2025-12-16 00:08:57 +0100

    add Task To entity

--- BackendProjectManagement/BackendProjectManagement/Mapper/TaskMapper.cs
+++ BackendProjectManagement/BackendProjectManagement/Mapper/TaskMapper.cs
@@ -1,6 +1,23 @@
-﻿namespace BackendProjectManagement.Mapper
+﻿using BackendProjectManagement.DTOs;
+using BackendProjectManagement.Models;
+
+namespace BackendProjectManagement.Mapper
 {
     public static class TaskMapper
     {
+
+        public static TaskItem ToEntity(CreateTaskDto dto)
+        {
+            return new TaskItem
+            {
+                Id = Guid.NewGuid(),
+                Title = dto.Title,
+                Description = dto.Description,
+                DueDate = dto.DueDate,
+                ProjectId = dto.ProjectId
+            };
+        }
+
+
     }
 }
