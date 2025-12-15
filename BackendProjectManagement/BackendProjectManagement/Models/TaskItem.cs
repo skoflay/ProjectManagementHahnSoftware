63f0069957c902767ae011b0d12c598e00c237c1
souhail <boullilsouhil@gmail.com>
2025-12-15 18:36:28 +0100

    Add TaskItem Model

---
+++ BackendProjectManagement/BackendProjectManagement/Models/TaskItem.cs
@@ -0,0 +1,15 @@
+﻿namespace BackendProjectManagement.Models
+{
+    public class TaskItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool IsCompleted { get; set; } = false;
+
+        public Guid ProjectId { get; set; }
+        public Project Project { get; set; } = null!;
+    }
+
+}
