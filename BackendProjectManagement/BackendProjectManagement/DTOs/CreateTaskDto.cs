3f73743a0aad377e2791c443d5c4947a9fd40d91
souhail <boullilsouhil@gmail.com>
2025-12-16 00:04:47 +0100

    add Task Dto & Create TaskDto

---
+++ BackendProjectManagement/BackendProjectManagement/DTOs/CreateTaskDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendProjectManagement.DTOs
+{
+    public class CreateTaskDto
+    {
+        [Required]
+        public string Title { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        [Required]
+        public Guid ProjectId { get; set; }
+    }
+}
---
+++ BackendProjectManagement/BackendProjectManagement/DTOs/TaskDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BackendProjectManagement.DTOs
+{
+    public class TaskDto
+    {
+        
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
