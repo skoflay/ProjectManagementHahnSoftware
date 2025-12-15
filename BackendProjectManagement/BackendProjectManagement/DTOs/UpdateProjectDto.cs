60d767a3db231c5a69691af1262e0884b3488790
souhail <boullilsouhil@gmail.com>
2025-12-15 20:20:06 +0100

    add update Dtos

---
+++ BackendProjectManagement/BackendProjectManagement/DTOs/UpdateProjectDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BackendProjectManagement.DTOs
+{
+    public class UpdateProjectDto
+    {
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+
+}
