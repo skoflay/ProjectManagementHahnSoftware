f1f6f315c4195fb3831531a44d332f015b83d40e
souhail <boullilsouhil@gmail.com>
2025-12-15 20:01:54 +0100

    inject Service

---
+++ BackendProjectManagement/BackendProjectManagement/Controllers/ProjectsController.cs
@@ -0,0 +1,19 @@
+﻿using BackendProjectManagement.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendProjectManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly IProjectService _projectService;
+
+        public ProjectsController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+    }
+}
