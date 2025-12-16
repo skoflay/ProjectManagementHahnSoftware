88e60cb9002f8a782450c85123572a57dfc0a015
souhail <boullilsouhil@gmail.com>
2025-12-16 22:12:48 +0100

    minor changes to project pages adding button details and routing to tasks page

--- frontend/src/pages/ProjectsPage.tsx
+++ frontend/src/pages/ProjectsPage.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import type { Project } from '../types/project.types';
 import { ProjectService } from '../services/project.service';
+import { useNavigate } from 'react-router-dom';
 
 const ProjectsPage = () => {
   const [projects, setProjects] = useState<Project[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     loadProjects();
@@ -18,7 +21,7 @@ const ProjectsPage = () => {
       setProjects(data);
     } catch (err) {
       setError('Failed to load projects');
-      console.log(err)
+      console.log(err);
     } finally {
       setLoading(false);
     }
@@ -38,12 +41,21 @@ const ProjectsPage = () => {
       <div className="row">
         {projects.map(project => (
           <div className="col-md-4 mb-3" key={project.id}>
-            <div className="card shadow-sm">
-              <div className="card-body">
+            <div className="card shadow-sm h-100">
+              <div className="card-body d-flex flex-column">
                 <h5 className="card-title">{project.title}</h5>
-                <p className="card-text">
+
+                <p className="card-text flex-grow-1">
                   {project.description || 'No description'}
                 </p>
+
+                
+                <button
+                  className="btn btn-primary btn-sm mt-3"
+                  onClick={() => navigate(`/projects/${project.id}/tasks`)}
+                >
+                  View Tasks
+                </button>
               </div>
             </div>
           </div>
