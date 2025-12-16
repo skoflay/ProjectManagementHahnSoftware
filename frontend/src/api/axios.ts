d36726fa82d67b3850b88a7c18db98a867775e85
souhail <boullilsouhil@gmail.com>
2025-12-16 21:09:43 +0100

    Default Config Cors

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: 'https://localhost:7033/api',
+  baseURL: 'http://localhost:7033/api',
   headers: {
     'Content-Type': 'application/json'
   }
