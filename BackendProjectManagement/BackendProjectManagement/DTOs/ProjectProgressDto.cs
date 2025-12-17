350e1dd856f55f9a8e4e3eb86a35df386924e287
souhail <boullilsouhil@gmail.com>
2025-12-17 14:58:32 +0100

    Progress DTO backend

---
+++ BackendProjectManagement/BackendProjectManagement/DTOs/ProjectProgressDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BackendProjectManagement.DTOs
+{
+    public class ProjectProgressDto
+    {
+        public int TotalTasks { get; set; }
+        public int CompletedTasks { get; set; }
+        public int ProgressPercentage { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
