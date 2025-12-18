ed8cb4a182df8333e2982441a893e600b673c328
souhail <boullilsouhil@gmail.com>
2025-12-18 22:40:21 +0100

    login dto attribute

--- BackendProjectManagement/BackendProjectManagement/DTOs/LoginDto.cs
+++ BackendProjectManagement/BackendProjectManagement/DTOs/LoginDto.cs
@@ -1,6 +1,13 @@
-﻿namespace BackendProjectManagement.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendProjectManagement.DTOs
 {
     public class LoginDto
     {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
     }
 }
