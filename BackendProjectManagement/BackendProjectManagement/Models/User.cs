db368629d729d0eba6769e39efef12a023a606d8
souhail <boullilsouhil@gmail.com>
2025-12-18 22:22:24 +0100

    add featureUsers

---
+++ BackendProjectManagement/BackendProjectManagement/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackendProjectManagement.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+        public ICollection<Project> Projects { get; set; } = new List<Project>();
+    }
+}
