f70f22712f41adc2fb623604a4ed79e819c2ddce
souhail <boullilsouhil@gmail.com>
2025-12-19 23:18:29 +0100

    AuthServiceTest

---
+++ BackendProjectManagement/BackendProjectManagement.Tests/Services/AuthServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using BackendProjectManagement;
+using BackendProjectManagement.Repositories;
+using BackendProjectManagement.Services;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BackendProjectManagement.Tests.Services
+{
+    public class AuthServiceTests
+    {
+        private readonly Mock<IAuthRepository> _authRepoMock;
+        private readonly Mock<ITokenService> _tokenServiceMock;
+        private readonly AuthService _authService;
+
+        public AuthServiceTests()
+        {
+            _authRepoMock = new Mock<IAuthRepository>();
+            _tokenServiceMock = new Mock<ITokenService>();
+            _authService = new IAuthService(_authRepoMock.Object, _tokenServiceMock.Object);
+        }
+
+        // Tests will go here
+    }
+}
