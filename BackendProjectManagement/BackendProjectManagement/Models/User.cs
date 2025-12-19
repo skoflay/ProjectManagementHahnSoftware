b14a434c7142421e6329b80280761e307dafc9ea
souhail <boullilsouhil@gmail.com>
2025-12-20 00:25:16 +0100

    add annotations to models

--- BackendProjectManagement/BackendProjectManagement/Models/User.cs
+++ BackendProjectManagement/BackendProjectManagement/Models/User.cs
@@ -9,12 +9,17 @@ namespace BackendProjectManagement.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        [MaxLength(150, ErrorMessage = "Email can't exceed 150 characters")]
         public string Email { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        [MaxLength(100, ErrorMessage = "Password can't exceed 100 characters")]
         public string Password { get; set; } = string.Empty;
 
+       
         public ICollection<Project> Projects { get; set; } = new List<Project>();
     }
 }
