9ad22d721c26c8154c519a5c3ec868d099fa78d1
souhail <boullilsouhil@gmail.com>
2025-12-20 00:01:13 +0100

    ProjectsControllerTests

---
+++ BackendProjectManagement/BackendProjectManagement.Tests/Controlles/ProjectsControllerTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackendProjectManagement.Tests.Controlles
+{
+    internal class ProjectsControllerTests
+    {
+    }
+}
